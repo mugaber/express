9a81e60e05e0776fcb31cb31fe812368cb3c3826
MuGaber <mohammedgaber273@gmail.com>
2020-03-07 19:31:28 +0200

    adding middlewares

--- hello.js
+++ hello.js
@@ -3,6 +3,20 @@ const app = express()
 const wiki = require('./wiki')
 const port = 3000
 
+// an example middleware
+const nextMiddlewre = (req, res, next) => {
+  next()
+}
+
+// with out the route, apply to all
+app.use(nextMiddlewre)
+
+// any method, sp route
+app.use('/wiki/', nextMiddlewre)
+
+// specific method, sp route
+app.get('/', nextMiddlewre)
+
 app.get('/', (req, res) => {
   res.send('home page')
 })
