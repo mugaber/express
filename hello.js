91822c32d8c7b22040bdbfae6c12a1a50de66049
MuGaber <mohammedgaber273@gmail.com>
2020-03-07 19:46:47 +0200

    error handling

--- hello.js
+++ hello.js
@@ -3,17 +3,21 @@ const app = express()
 const wiki = require('./wiki')
 const port = 3000
 
-// serving static files from the public folder
 app.use(express.static('public'))
-
-// using another file
 app.use(express.static('media'))
+app.use('/wiki', wiki)
 
 app.get('/', (req, res) => {
   res.send('home page')
 })
 
-app.use('/wiki', wiki)
+// handling errors middleware have extra argument at the start err
+// err handlers should be the last middlewares to catch all errors
+// there is built in error handling but does not include 404, ...
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something went wrong')
+})
 
 app.listen(port, () => {
   console.log('Server Running on port ' + port)
