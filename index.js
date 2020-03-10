24a0bd654203a4cbfbe66f29de289500dc8306c4
MuGaber <mohammedgaber273@gmail.com>
2020-03-10 20:15:35 +0200

    some mongoose queries

--- index.js
+++ index.js
@@ -1,55 +1,66 @@
 const bodyParser = require('body-parser')
 const express = require('express')
-require('dotenv').config() // to be able to use .env shell file
+require('dotenv').config()
 
+// initlaize the app
 const app = express()
 const port = 3000
 
-// connect to mongo database
+// connect to db
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
+// get connection
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error'))
 db.once('open', function() {
   console.log('database connected')
 })
 
-// creating an instance using the model
+// query db
 const Person = require('./models/person')
-const muhamed = new Person({ name: 'muhamed', age: 23 })
 
-// save to the db
-muhamed.save(function(err, data) {
-  if (err) return console.error(err)
-  console.log('saved to db', data)
-})
+const asyncQueries = async function() {
+  console.log('running async db call')
+  let id = ''
 
-// creating a lot of instances and saving them
-const arrayOfPeople = [
-  { name: 'ahmed', age: 20 },
-  { name: 'am', age: 30 }
-]
-Person.create(arrayOfPeople, callback)
+  await Person.find(function(err, data) {
+    if (err) return console.error(err)
+    id = data[0]._id
+    console.log('Person documents \n', data)
+  })
 
-// access the data
-const callback = function(err, data) {
-  if (err) return console.error(err)
-  console.log(data)
+  await Person.findById(id, function(err, data) {
+    if (err) console.error(err)
+    data.name = 'muhamed'
+    data.save(function(err, data) {
+      if (err) console.error(err)
+      console.log('saved doc', data)
+    })
+  })
+
+  await Person.findOneAndUpdate(
+    { name: 'muhamed' },
+    { name: 'muhammed' },
+    { new: true, useFindAndModify: false },
+    (err, data) => console.log('find one and update', data)
+  )
+
+  // query chaining, because there is no callback we can call exec
+  await Person.find()
+    .select('-favoriteFoods')
+    .sort({ name: 'asc' })
+    .limit(2)
+    .exec(function(err, data) {
+      if (err) return console.error(err)
+      console.log(data)
+    })
 }
 
-Person.find(callback) // returns array of documents
-
-Person.find({ name: 'muhamed' }, callback) // array of matched docs
-
-// logger
-app.get('/', function(req, res, next) {
-  console.log(req.method, req.path, req.ip)
-  next()
-})
+asyncQueries()
 
 // serving static files
 app.use(express.static(__dirname + '/public'))
@@ -58,5 +69,3 @@ app.use(express.static(__dirname + '/public'))
 app.listen(port, function(req, res) {
   console.log(`Server running on port ${port}`)
 })
-
-// tab nine
